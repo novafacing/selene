5f02dbf4bd9b47ea0452d9d17d6ff36fe0eec0c2
Cong <congusbongus@gmail.com>
2022-11-22 14:24:21 +1100

    Add moonscript.init to package path for fused files

--- conf.lua
+++ conf.lua
@@ -1,9 +1,11 @@
-package.path = package.path .. ";./lib/moonscript/?.lua"
+-- https://love2d.org/wiki/Config_Files
+package.path = package.path .. ";./lib/moonscript/?.lua;./lib/moonscript/moonscript.init"
 package.path = package.path .. ";./lib/lulpeg/?.lua"
 
 function love.conf(t)
     require("lib.moonscript")
     t.console = true
+    t.window.title = "My Selene Game"
     t.window.width = 800
     t.window.height = 480
 end
